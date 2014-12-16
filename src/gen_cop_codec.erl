17b1f2cf0d64e0be54aa91b55e520443daed5fd5
Takeru Ohta <phjgt308@gmail.com>
2014-12-17 04:05:55 +0900

    Add unit tests for gen_cop:start/4

--- src/gen_cop_codec.erl
+++ src/gen_cop_codec.erl
@@ -1,17 +1,71 @@
+%% @copyright 2014, Takeru Ohta <phjgt308@gmail.com>
+%%
+%% @doc Encoder/Decoder Interface
+%%
+%% TODO: doc
 -module(gen_cop_codec).
 
+%%----------------------------------------------------------------------------------------------------------------------
+%% Exported API
+%%----------------------------------------------------------------------------------------------------------------------
+-export([make/2, make/3]).
 -export([encode/2, decode/2]).
+
 -export_type([codec/0]).
+-export_type([codec_module/0, codec_state/0]).
+-export_type([encode_fun/0, decode_fun/0]).
+-export_type([encode_result/0, decode_result/0]).
 
--type codec() :: term().
+%%----------------------------------------------------------------------------------------------------------------------
+%% Macros & Records & Types
+%%----------------------------------------------------------------------------------------------------------------------
+-define(CODEC, ?MODULE).
 
--callback encode([gen_cop:data()], codec()) -> {ok, iodata(), codec()} | {error, Reason::term(), codec()}.
--callback decode(binary(), codec()) -> {ok, [gen_cop:data()], codec()} | {error, Reason::term(), codec()}.
+-record(?CODEC,
+        {
+          state  :: codec_state(),
+          encode :: encode_fun(),
+          decode :: decode_fun()
+        }).
 
-encode(DataList, {Mod, State}) ->
-    Result = {_, _, State2} = Mod:encode(DataList, State),
-    setelement(3, Result, {Mod, State2}).
+-opaque codec() :: #?CODEC{}.
 
-decode(Bin, {Mod, State}) ->
-    Result = {_, _, State2} = Mod:decode(Bin, State),
-    setelement(3, Result, {Mod, State2}).
+-type codec_module() :: module().
+-type codec_state() :: term().
+
+-type encode_fun() :: fun (([gen_cop:message()], codec_state()) -> encode_result()).
+-type decode_fun() :: fun ((binary(), codec_state()) -> decode_result()).
+
+-type encode_result() :: {ok, iodata(), codec_state()} | {error, Reason::term(), codec_state()}.
+-type decode_result() :: {ok, [gen_cop:message()], codec_state()} | {error, Reason::term(), codec_state()}.
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Callback API
+%%----------------------------------------------------------------------------------------------------------------------
+-callback encode([gen_cop:message()], codec_state()) -> encode_result().
+-callback decode(binary(), codec_state()) -> decode_result().
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Exported Functions
+%%----------------------------------------------------------------------------------------------------------------------
+-spec make(codec_module(), codec_state()) -> codec().
+make(Module, State) ->
+    make(State, fun Module:encode/2, fun Module:decode/2).
+
+-spec make(codec_state(), encode_fun(), decode_fun()) -> codec().
+make(State, EncodeFun, DecodeFun) ->
+    #?CODEC{state = State, encode = EncodeFun, decode = DecodeFun}.
+
+-spec encode([gen_cop:message()], codec()) -> encode_result().
+encode(Messages, Codec) ->
+    case (Codec#?CODEC.encode)(Messages, Codec#?CODEC.state) of
+        {ok, Encoded, State}   -> {ok, Encoded, Codec#?CODEC{state = State}};
+        {error, Reason, State} -> {error, Reason, Codec#?CODEC{state = State}}
+    end.
+
+-spec decode(binary(), codec_state()) -> decode_result().
+decode(Bin, Codec) ->
+    case (Codec#?CODEC.decode)(Bin, Codec#?CODEC.state) of
+        {ok, Messages, State}  -> {ok, Messages, Codec#?CODEC{state = State}};
+        {error, Reason, State} -> {error, Reason, Codec#?CODEC{state = State}}
+    end.
---
+++ test/gen_cop_tests.erl
@@ -0,0 +1,105 @@
+%% coding: latin-1
+%% @copyright 2014, Takeru Ohta <phjgt308@gmail.com>
+-module(gen_cop_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Macros
+%%----------------------------------------------------------------------------------------------------------------------
+-define(assertDown(Pid, ExpectedReason),
+        (fun () ->
+                 __Monitor = monitor(process, Pid),
+                 receive
+                     {'DOWN', __Monitor, _, _, __Reason} -> ?assertMatch(ExpectedReason, __Reason)
+                 after 50 -> ?assert(timeout)
+                 end
+         end)()).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Unit Tests
+%%----------------------------------------------------------------------------------------------------------------------
+start_test_() ->
+    [
+     {"プロセス起動: 基本的な起動",
+      fun () ->
+              {ok, Socket} = gen_tcp:listen(0, []),
+              Codec = make_dummy_codec(),
+
+              Result = gen_cop:start(Socket, Codec, []),
+              ?assertMatch({ok, _}, Result),
+              {ok, Pid} = Result,
+
+              true = exit(Pid, kill),
+              ?assertDown(Pid, killed)
+      end},
+     {"プロセス起動: リンク付きの起動",
+      fun () ->
+              {ok, Socket} = gen_tcp:listen(0, []),
+              Codec = make_dummy_codec(),
+
+              Result = gen_cop:start_link(Socket, Codec, []),
+              ?assertMatch({ok, _}, Result),
+              {ok, Pid} = Result,
+
+              _ = process_flag(trap_exit, true),
+              true = exit(Pid, kill),
+              ?assertDown(Pid, killed)
+      end},
+     {"プロセス起動: 名前付き",
+      fun () ->
+              Name = hoge_name,
+              {ok, Socket} = gen_tcp:listen(0, []),
+              Codec = make_dummy_codec(),
+
+              Result = gen_cop:start(Socket, Codec, [], [{name, {local, Name}}]),
+              ?assertMatch({ok, _}, Result),
+              {ok, Pid} = Result,
+
+              ?assertEqual(Pid, whereis(Name)),
+
+              true = exit(Pid, kill),
+              ?assertDown(Pid, killed)
+      end},
+     {"プロセス起動: 名前の衝突",
+      fun () ->
+              Name = hoge_name,
+              {ok, Socket} = gen_tcp:listen(0, []),
+              Codec = make_dummy_codec(),
+
+              {ok, Pid} = gen_cop:start(Socket, Codec, [], [{name, {local, Name}}]),
+
+              %% 衝突
+              ?assertEqual({error, {already_started, Pid}}, gen_cop:start(Socket, Codec, [], [{name, {local, Name}}])),
+
+              true = exit(Pid, kill),
+              ?assertDown(Pid, killed)
+      end},
+     {"プロセス起動: asyncオプションが有効 (初期化完了まで待たない)",
+      fun () ->
+              Codec = make_dummy_codec(),
+              UndefinedHandlerSpecs = [{undefined_module, []}], % 不正なハンドラ
+
+              %% `{async, false}'の場合(デフォルト)は、start関数呼び出しに失敗する
+              {ok, Socket0} = gen_tcp:listen(0, []),
+              ?assertMatch({error, {'EXIT', [{exception, error, undef} | _]}},
+                           gen_cop:start(Socket0, Codec, UndefinedHandlerSpecs)),
+
+              %% `{async, true}'の場合は、起動は初期化処理を待たずに成功する
+              {ok, Socket1} = gen_tcp:listen(0, []),
+              Result = gen_cop:start(Socket1, Codec, UndefinedHandlerSpecs, [{async, true}]),
+              ?assertMatch({ok, _}, Result),
+              {ok, Pid} = Result,
+
+              ?assertDown(Pid, {'EXIT', [{exception, error, undef} | _]})
+      end}
+    ].
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Internal Functions
+%%----------------------------------------------------------------------------------------------------------------------
+-spec make_dummy_codec() -> gen_cop_codec:codec().
+make_dummy_codec() ->
+    gen_cop_codec:make([],
+                       fun (_, Enc) -> {ok, [], Enc} end,
+                       fun (_, Dec) -> {ok, [], Dec} end).
