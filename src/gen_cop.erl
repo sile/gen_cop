e2aefc110d915c54545afc4fe650b2aed0c1aa18
Takeru Ohta <phjgt308@gmail.com>
2018-05-08 12:15:23 +0900

    Support logi-0.5.x

--- src/gen_cop.erl
+++ src/gen_cop.erl
@@ -14,6 +14,7 @@
 -export_type([start_err/0]).
 -export_type([ack_fun/0]).
 -export_type([otp_name/0, otp_ref/0]).
+-export_type([logi_logger/0]).
 
 -export([reply/2]).
 -export([send/2]).
@@ -55,8 +56,11 @@
                    | {spawn_opt, [term()]} % TODO: more specific typespec
                    | {timeout, timeout()}
                    | {debug, [term()]} % TODO: more specific typespec
-                   | {logger, logi:context()}.
+                   | {logger, logi_logger()}.
 %% TODO: doc
+
+-type logi_logger() :: term().
+%% logi:context() in v0.1.12, logi:logger() in v0.5.x
 
 -type start_err() :: {already_started, pid()} | timeout | term().
 
@@ -121,7 +125,7 @@ reply(From, Reply) ->
     ok.
 
 %% TODO: rename: get_default_logger
--spec get_logger(otp_ref()) -> logi:context().
+-spec get_logger(otp_ref()) -> logi_logger().
 get_logger(ServerRef) ->
     call(ServerRef, get_logger).
 
--- src/gen_cop_server.erl
+++ src/gen_cop_server.erl
@@ -154,7 +154,7 @@ init(Parent, _, AckFun, SyncFun, {Socket, Codec, Handlers, Options}) ->
             _  = SyncFun({ok, self()}),
             {ok, [{sndbuf, SndBuf}, {recbuf, RecBuf}]} = inet:getopts(Socket, [sndbuf, recbuf]),
             ok = inet:setopts(Socket, [{active, ?ACTIVE_COUNT}, {buffer, max(SndBuf, RecBuf)}]),
-            ok = logi:save_context(proplists:get_value(logger, Options, logi:make_context())), % TODO: 不要な時には保存しない
+            _ = logi:save_context(proplists:get_value(logger, Options, logi:make_context())), % TODO: 不要な時には保存しない
 
             State =
                 #state{
